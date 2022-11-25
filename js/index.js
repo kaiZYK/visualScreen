0dc11ac03bea14b4607963471a30e0c92118747c
xiaokai <1020849724@qq.com>
2022-11-25 18:31:55 +0800

    项目完成

--- js/index.js
+++ js/index.js
@@ -218,19 +218,11 @@ for (let i = 0; i < controlTitleItem.length; i++) {
 
   let XNData = [
     [{ name: "西宁" }, { name: "北京", value: 100 }],
-    [{ name: "西宁" }, { name: "上海", value: 100 }],
-    [{ name: "西宁" }, { name: "广州", value: 100 }],
-    [{ name: "西宁" }, { name: "西安", value: 100 }],
-    [{ name: "西宁" }, { name: "银川", value: 100 }],
     [{ name: "西宁" }, { name: "郑州", value: 100 }],
   ];
 
   let YCData = [
     [{ name: "银川" }, { name: "北京", value: 100 }],
-    [{ name: "银川" }, { name: "广州", value: 100 }],
-    [{ name: "银川" }, { name: "上海", value: 100 }],
-    [{ name: "银川" }, { name: "西安", value: 100 }],
-    [{ name: "银川" }, { name: "西宁", value: 100 }],
     [{ name: "银川" }, { name: "郑州", value: 100 }],
   ];
   let ZZData = [
@@ -888,5 +880,88 @@ order.addEventListener("mouseleave", function () {
   });
 })();
 
-
 /* 进度统计部分 */
+(function () {
+  let progressEcharts = document.getElementsByClassName("progressEcharts")[0];
+  let myChart = echarts.init(progressEcharts);
+  let compositeScore = 0.78;
+  let option = {
+    graphic: [
+      {
+        type: "group",
+        left: "center",
+        top: "58%",
+        children: [
+          {
+            type: "text",
+            z: 100,
+            left: "10",
+            top: "middle",
+            style: {
+              fill: "#fff",
+              text: "综合评分",
+              font: "16px PingFangSC-Regular",
+            },
+          },
+        ],
+      },
+    ],
+    series: [
+      {
+        type: "liquidFill", //水位图
+        radius: "90%", //显示比例
+        center: ["50%", "50%"], //中心点
+        data: [compositeScore], // data个数代表波浪数
+        color: ["#82d5e7"], //波浪颜色
+        backgroundStyle: {
+          color: {
+            type: "linearGradient",
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+              {
+                offset: 1,
+                color: "rgba(17,65,86,0.64)",
+              },
+              {
+                offset: 0.51,
+                color: "rgba(32,70,84,0)",
+              },
+              {
+                offset: 0,
+                color: "rgba(99,254,254,0.35)",
+              },
+            ],
+            globalCoord: false,
+          },
+        },
+        label: {
+          //标签设置
+          position: ["50%", "50%"],
+          formatter: compositeScore * 100 + "%", //显示文本
+          fontSize: "32px",
+          fontFamily: "DINAlternate-Bold",
+          fontWeight: "bold",
+          color: "#fff",
+        },
+        outline: {
+          show: true,
+          borderDistance: 5,
+          itemStyle: {
+            borderColor: "#1B5151",
+            borderWidth: 1,
+          },
+        },
+      },
+    ],
+  };
+  myChart.setOption(option);
+  window.addEventListener("load", function () {
+    myChart.resize();
+  });
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+})();
